a57528d7a0032aeae778dba04696a5064e1b8dc1
Bhuvan506 <jsbhuvan@gmail.com>
2023-12-11 13:16:23 +0530

    logger test

--- node-server/index.js
+++ node-server/index.js
@@ -9,7 +9,7 @@ const path = require('path');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://database:127017/taskM');
+    await mongoose.connect('mongodb://127.0.0.1:127017/taskM');
     console.log('db connected');
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
